cd5145edc100263d85b43c22025bc1d9998ec968
Giles Alexander <giles.alexander@gmail.com>
2011-12-02 19:16:50 -0800

    Handles paths containing filters.
    This required rewriting almost the entire json-path code,
    progressively.

--- src/json_path/core.clj
+++ src/json_path/core.clj
@@ -1,6 +1,8 @@
 (ns json-path
   [:use [midje.sweet]])
 
+(unfinished )
+
 (defn- extract-sub-tree [start end stream]
   (take-while #(not (= end %)) (drop-while #(= start %) stream)))
 
@@ -17,10 +19,7 @@
 (fact
   (parse-expr  '("@" "." "foo" "=" "\"" "baz" "\""))
   =>
-  [:eq [:path [[:current] [:child] [:key "foo"]]] [:val "baz"]]
-  (provided
-    (parse '("@" "." "foo")) => [:path [[:current] [:child] [:key "foo"]]]
-    (parse '("\"" "baz" "\"")) => [:val "baz"]))
+  [:eq [:path [[:current] [:child] [:key "foo"]]] [:val "baz"]])
 
 (defn- parse-indexer [remaining]
   (let [next (first remaining)]
@@ -85,17 +84,17 @@
                                                                         [:val "baz"]]]
                                                     [:path [[:child] [:key "hello"]]]]])
 
-(defn- eval-expr [[expr-type & operands] object]
+(defn- eval-expr [[expr-type & operands :as expr] object]
   (cond
    (= expr-type :eq) (apply = (map #(eval-expr % object) operands))
    (= expr-type :val) (first operands)
-   (= expr-type :path) (eval-path operands object)))
+   (= expr-type :path) (walk expr object)))
 
 (facts
   (eval-expr [:eq [:val "a"] [:val "b"]] {}) => falsey
   (eval-expr [:eq [:val "a"] [:val "a"]] {}) => truthy
-  (eval-expr [:path [:key "foo"]] {:foo "bar"}) => "bar"
-  (eval-expr [:eq [:path [:key "foo"]] [:val "bar"]] {:foo "bar"}) => truthy)
+  (eval-expr [:path [[:key "foo"]]] {:foo "bar"}) => "bar"
+  (eval-expr [:eq [:path [[:key "foo"]]] [:val "bar"]] {:foo "bar"}) => truthy)
 
 (defn- select-by [[opcode & operands :as obj-spec] object]
   (cond
@@ -113,66 +112,81 @@
   (sort (select-by [:key "*"] {:hello "world", :foo "bar"})) => ["bar", "world"]
   (sort (select-by [:key "*"] [{:hello "world"}, {:foo "bar"}])) => ["bar", "world"])
 
-(defn- path-filter? [[opcode & operands]]
-  (not (empty? (filter #(= opcode %) [:root :current :child :all-children :index :filter]))))
+(defn obj-aggregator [obj]
+  (let [obj-vals (vec (filter map? (vals obj)))
+        children (flatten (map obj-aggregator obj-vals))]
+    (vec (concat obj-vals children))))
+
+(defn walk-path [[next & parts] object]
+  (cond
+   (nil? next) object
+   (= [:root] next) (walk-path parts object)
+   (= [:child] next) (walk-path parts object)
+   (= [:current] next) (walk-path parts object)
+   (= [:all-children] next) (walk-path parts (vec (concat [object] (obj-aggregator object))))
+   (= :key (first next)) (walk-path parts (select-by next object))))
+
+(fact
+  (walk-path [[:root]] ...obj...) => ...obj...
+  (walk-path [[:root] [:child] [:key "foo"]] {:foo "bar"}) => "bar"
+  (walk-path [[:all-children]] {:foo "bar" :baz {:qux "zoo"}}) => [{:foo "bar" :baz {:qux "zoo"}},
+                                                                   {:qux "zoo"}])
+
+(defn walk-selector [sel-expr object]
+  (cond
+   (= :index (first sel-expr)) (let [sel (nth sel-expr 1)]
+                                 (if (= "*" sel)
+                                   object
+                                   (nth object (Integer/parseInt sel))))
+   (= :filter (first sel-expr)) (filter #(eval-expr (nth sel-expr 1) %) object))))
+
+(fact
+  (walk-selector [:index "1"] ["foo", "bar", "baz"]) => "bar"
+  (walk-selector [:index "*"] ...array...) => ...array...
+  (walk-selector [:filter [:eq [:path [[:current] [:child] [:key "bar"]]] [:val "baz"]]]
+                 [{:bar "wrong"} {:bar "baz"}]) => [{:bar "baz"}])
+
+(defn- walk [[opcode operand continuation] object]
+  (let [down-obj (cond
+         (= opcode :path) (walk-path operand object)
+         (= opcode :selector) (walk-selector operand object))]
+    (if continuation
+      (walk continuation down-obj)
+      down-obj)))
 
 (facts
-  (path-filter? [:key "hello"]) => falsey
-  (path-filter? [:root]) => truthy
-  (path-filter? [:current]) => truthy
-  (path-filter? [:child]) => truthy
-  (path-filter? [:all-children]) => truthy
-  (path-filter? [:index "3"]) => truthy
-  (path-filter? [:filter :expression]) => truthy)
-
-(defn- walk [[opcode & operands] object]
-  (let [obj-aggregator (fn obj-aggregator [obj]
-                         (let [obj-vals (vec (filter map? (vals obj)))
-                               children (flatten (map obj-aggregator obj-vals))]
-                           (vec (concat obj-vals children))))]
-    (cond
-     (or (= opcode :root) (= opcode :child) (= opcode :current)) object
-     (= opcode :all-children) (vec (concat [object] (obj-aggregator object)))
-     (= opcode :index) (let [selector (first operands)]
-                           (cond
-                            (= selector "*") object
-                            :else (nth object (Integer/parseInt (first operands)))))
-     (= opcode :filter) (filter #(eval-expr (first operands) %) object))))
-
-(facts
-  (walk [:root] ...json...) => ...json...
-  (walk [:child] ...json...) => ...json...
-  (walk [:current] ...json...) => ...json...
-  (walk [:all-children] {:hello {:world "foo"},
-                         :baz {:world "bar",
-                               :quuz {:world "zux"}}}) => [{:hello {:world "foo"}
-                                                            :baz {:world "bar", :quuz {:world "zux"}}},
-                                                           {:world "foo"},
-                                                           {:world "bar",
-                                                            :quuz {:world "zux"}},
-                                                           {:world "zux"}]
-  (walk [:index "1"] ["foo", "bar", "baz"]) => "bar"
-  (walk [:index "*"] ["foo", "bar", "baz"]) => ["foo", "bar", "baz"]
-  (walk [:filter [:eq
-                  [:path [:current]
-                   [:child]
-                   [:key "bar"]]
-                  [:val "baz"]]] [{:bar "wrong"} {:bar "baz"}]) => [{:bar "baz"}])
-
-(defn- eval-path [path object]
-  (loop [parts path
-         target object]
-    (if (empty? parts)
-      target
-      (let [next (first parts)]
-        (cond
-         (path-filter? next) (recur (rest parts)
-                                    (walk next target))
-         :else (recur (rest parts)
-                      (select-by next target)))))))
+  (walk [:path [[:root]]] ...json...) => ...json...
+  (walk [:path [[:child]]] ...json...) => ...json...
+  (walk [:path [[:current]]] ...json...) => ...json...
+  (walk [:path [[:key "foo"]]] {:foo "bar"}) => "bar"
+  (walk [:path [[:all-children]]]
+        {:hello {:world "foo"},
+         :baz {:world "bar",
+               :quuz {:world "zux"}}}) => [{:hello {:world "foo"},
+         :baz {:world "bar", :quuz {:world "zux"}}},
+        {:world "foo"},
+        {:world "bar",
+         :quuz {:world "zux"}},
+        {:world "zux"}]
+  (walk [:selector [:index "1"]] ["foo", "bar", "baz"]) => "bar"
+  (walk [:selector [:index "*"]] ...array...) => ...array...
+  (walk [:selector [:filter [:eq
+                             [:path [[:current]
+                                     [:child]
+                                     [:key "bar"]]]
+                             [:val "baz"]]]]
+        [{:bar "wrong"} {:bar "baz"}]) => [{:bar "baz"}]
+  (walk [:path [[:root] [:child] [:key "foo"]]
+         [:selector [:filter [:eq [:path [[:current]
+                                          [:child]
+                                          [:key "bar"]]]
+                              [:val "baz"]]]
+          [:path [[:child] [:key "hello"]]]]]
+        {:foo [{:bar "wrong" :hello "goodbye"}
+               {:bar "baz" :hello "world"}]})) => ["world"]
 
 (defn at-path [path object]
-  (eval-path (parse-path path) object))
+  (walk (parse-path path) object))
 
 (facts
   (at-path "$" ...json...) => ...json...
@@ -187,11 +201,4 @@
   (at-path "$.foo[*]" {:foo ["a", "b", "c"]}) => ["a", "b", "c"]
   (at-path "$.foo[?(@.bar=\"baz\")].hello"
            {:foo [{:bar "wrong" :hello "goodbye"}
-                  {:bar "baz" :hello "world"}]}) => ["world"]
-  (provided
-    (parse-path "$.foo[?(@.bar=\"baz\")].hello") => [[:root] [:child] [:key "foo"]
-                                                     [:filter [:eq [:path [:current]
-                                                                    [:child]
-                                                                    [:key "bar"]]
-                                                               [:val "baz"]]]
-                                                     [:key "hello"]]))
+                  {:bar "baz" :hello "world"}]}) => ["world"])
