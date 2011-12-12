4d515194a58faafa4066cc3d030dd90c01a61b47
Giles Alexander <giles.alexander@gmail.com>
2011-12-12 17:21:53 -0600

    Added a project URL.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject json-path "0.2.0"
   :description "JSON Path for Clojure data structures"
+  :url "http://github.com/gga/json-path"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [midje "1.3.0"]
