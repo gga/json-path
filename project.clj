15957b4c3ac7bb0b84c29e00eff06f002ad9c57a
Giles Alexander <giles.alexander@gmail.com>
2016-08-22 15:03:36 +0100

    Set the license

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject json-path "0.3.0"
   :description "JSON Path for Clojure data structures"
   :url "http://github.com/gga/json-path"
+  :license "Eclipse Public License 1.0"
   :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]
                                   [lein-midje "3.0.0"]]}})
