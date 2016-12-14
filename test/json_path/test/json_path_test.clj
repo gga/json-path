a8c75b94c89ff74356458e286e89939ac3fd0b17
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-12-15 00:12:44 +0800

    API to query keys alongside values

--- test/json_path/test/json_path_test.clj
+++ test/json_path/test/json_path_test.clj
@@ -21,3 +21,13 @@
   (at-path "$.foo[?(@.id=$.id)].text"
            {:id 45, :foo [{:id 12, :text "bar"},
                           {:id 45, :text "hello"}]}) => ["hello"])
+
+(facts
+ (query "$..world" {:baz {:world "bar",
+                          :quuz {:world "zux"}}})  => '(["bar" [:baz :world]] ["zux" [:baz :quuz :world]])
+ (query "$.foo[*]" {:foo ["a", "b", "c"]}) => '(["a" [:foo 0]] ["b" [:foo 1]] ["c" [:foo 2]])
+ (query "$.hello" {:hello "world"}) => ["world" [:hello]]
+ (query "$" {:hello "world"}) => [{:hello "world"} []]
+ (query "$.foo[?(@.bar=\"baz\")].hello"
+        {:foo [{:bar "wrong" :hello "goodbye"}
+               {:bar "baz" :hello "world"}]}) => '(["world" [:foo 1 :hello]]))
