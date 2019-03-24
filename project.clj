1f6a7251ecd63038cade722dc3f63d856f4f7508
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-03-24 20:28:39 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://github.com/gga/json-path"
   :license "Eclipse Public License 1.0"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+  :profiles {:dev {:dependencies [[midje "1.9.6"]]
                    :plugins [[lein-midje "3.2.1"]]}})
