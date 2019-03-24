87faecffc682f44969b560dbe8b702904761f5d0
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-03-24 20:29:33 +0100

    Upgrade to Clojure 1.10.0

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "JSON Path for Clojure data structures"
   :url "http://github.com/gga/json-path"
   :license "Eclipse Public License 1.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles {:dev {:dependencies [[midje "1.9.6"]]
                    :plugins [[lein-midje "3.2.1"]]}})
