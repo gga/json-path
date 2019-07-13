967908956657c3c4b80591beebd31a88cf74cc5e
Christoph Burgmer <christoph.burgmer@gmail.com>
2019-07-13 22:25:12 +0200

    Warn on regressions which might be intentional changes though (status fail and open from the regression report)

--- test/json_path/test/regression_test.clj
+++ test/json_path/test/regression_test.clj
@@ -16,3 +16,21 @@
                          scalar first)
                        (json-path/at-path selector document))))))
        doall))
+
+;; This section is for warning us if we are moving away from previously
+;; recorded results which however are not backed by a consensus based
+;; on https://github.com/cburgmer/json-path-comparison
+(deftest warning-on-changes-for-non-conforming-queries-based-on-consensus
+  (->> (queries_from_suite "test/Clojure_json-path.yaml")
+       (filter (fn [{status :status}] (not= status "pass")))
+       (map (fn [{:keys [selector document result status scalar id]}]
+              (testing id
+                (let [last-recorded (cond-> result
+                                      scalar first)
+                      current (json-path/at-path selector document)]
+                  (when (not= last-recorded
+                              current)
+                    (println (format "Warning: result has changed for %s: %s (status %s)" id selector status))
+                    (println (format "         was    %s" (pr-str last-recorded)))
+                    (println (format "         now is %s" (pr-str current))))))))
+       doall))
