745e3af824c802e2c49eaa346a8773e66934b88a
Christoph Burgmer <christoph.burgmer@gmail.com>
2022-04-27 22:27:54 +0200

    Fix dependency issue now that upstream published a new version

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev {:dependencies [[midje "1.9.6"]
-                                  [org.flatland/ordered "1.5.7"] ;; https://github.com/owainlewis/yaml/issues/28
                                   [org.clojure/data.json "0.2.7"]
-                                  [io.forward/yaml "1.0.9"]]
+                                  [io.forward/yaml "1.0.11"]]
                    :plugins [[lein-midje "3.2.1"]]}})
