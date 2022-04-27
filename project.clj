855c792a8cd9b520b593664ca4e3d34870e1f17d
Chris Oakman <chris@oakmac.com>
2022-04-27 13:12:39 -0500

    correct license in project.clj file

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject json-path "2.1.0"
   :description "JSON Path for Clojure data structures"
   :url "http://github.com/gga/json-path"
-  :license "Eclipse Public License 1.0"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :profiles {:dev {:dependencies [[midje "1.9.6"]
                                   [org.flatland/ordered "1.5.7"] ;; https://github.com/owainlewis/yaml/issues/28
